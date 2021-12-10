265561f3c53c14bbe3920a5c4de3c5b82cd22a25
abazov73 <abazov73@gmail.com>
2021-12-11 00:02:46 +0400

    Little ghost movement fix. Just to save it before flickering fix

--- Pacman_Game/Game.cpp
+++ Pacman_Game/Game.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
 #include "Game.h"
 #include "framework.h"
 //Карта
@@ -48,19 +50,27 @@ int map[HEIGHT][WIDTH] = {
 struct Ghost {
 	int number;
 	int direction;
+	int onFoodFlag;
 };
 
-Ghost ghost1 = { 1, RIGHT };
-Ghost ghost2 = { 2, UP };
-Ghost ghost3 = { 3, UP };
-Ghost ghost4 = { 4, LEFT };
+Ghost ghost1 = { 1, RIGHT, 0};
+Ghost ghost2 = { 2, UP, 0};
+Ghost ghost3 = { 3, UP, 0};
+Ghost ghost4 = { 4, LEFT, 0};
 
 Ghost ghosts[4] = { ghost1, ghost2, ghost3, ghost4 };
 
 int objectSizeX = 50;
 int objectSizeY = 50;
 
-int score = 0;
+int score = 1;
+
+HFONT hFont = CreateFont(25,
+	0, 0, 0, 0, 0, 0, 0,
+	DEFAULT_CHARSET,
+	0, 0, 0, 0,
+	L"Courier New"
+);
 
 void DrawMap(HDC hdc) {
 	HBRUSH hBrushEmpty = CreateSolidBrush(RGB(0, 0, 0)); // кисть пустого поля - черный
@@ -71,13 +81,15 @@ void DrawMap(HDC hdc) {
 
 	HBRUSH hBrushEnemy1 = CreateSolidBrush(RGB(255, 0, 0)); // кисть призрака №1 - красный 
 	
-	HBRUSH hBrushEnemy2 = CreateSolidBrush(RGB(128, 166, 255));
+	HBRUSH hBrushEnemy2 = CreateSolidBrush(RGB(128, 166, 255)); // кисть призрака №2 - голубой
 
-	HBRUSH hBrushEnemy3 = CreateSolidBrush(RGB(255, 128, 0));
+	HBRUSH hBrushEnemy3 = CreateSolidBrush(RGB(255, 128, 0)); // кисть призрака №3 - оранжевый
 
-	HBRUSH hBrushEnemy4 = CreateSolidBrush(RGB(0xFF, 0xC0, 0xCB));
+	HBRUSH hBrushEnemy4 = CreateSolidBrush(RGB(0xFF, 0xC0, 0xCB)); // кисть призрака №4 - розовый
 
 	HBRUSH hBrushFood = CreateSolidBrush(RGB(246, 138, 0)); // кисть еды - оранжевый
+
+	SetBkMode(hdc, TRANSPARENT);
 
 	int i, j;
 	i = 0;
@@ -133,6 +145,20 @@ void DrawMap(HDC hdc) {
 		}
 		i++;
 	}
+
+	SelectObject(hdc, hFont);
+	SetTextColor(hdc, RGB(255, 255, 255));
+	TCHAR  scoreText[] = _T("Счёт: ");
+	TextOut(hdc, 5, (objectSizeY + 2) * (HEIGHT-1), (LPCWSTR)scoreText, _tcslen(scoreText));
+
+
+	char sScore[5];
+	TCHAR  tsScore[5];
+	sprintf(sScore, "%d", score);
+	OemToChar(sScore, tsScore);
+	TextOut(hdc, 115, (objectSizeY + 2) * (HEIGHT-1), (LPCWSTR)tsScore, _tcslen(tsScore));
+
+
 	DeleteObject(hBrushEmpty);
 	DeleteObject(hBrushWall);
 	DeleteObject(hBrushPlayer);
@@ -236,7 +262,7 @@ void ChangeGhostDirection(int ghostNum, int direction, int i, int j) {
 	int randomNum = rand() % 2;
 	switch (direction) {
 	case UP:
-		if (map[i][j - 1] > -1 && map[i][j + 1] > 0 && map[i][j - 1] != WALL && map[i][j + 1] != WALL && map[i][j - 1] < 4 && map[i][j + 1] < 4) { //фикисить по этому образцу
+		if (map[i][j - 1] > -1 && map[i][j + 1] > -1 && map[i][j - 1] != WALL && map[i][j + 1] != WALL && map[i][j - 1] < 4 && map[i][j + 1] < 4) { //фикисить по этому образцу
 			randomNum = rand() % 2;
 			if (randomNum == 0) direction = LEFT;
 			else direction = RIGHT;
@@ -256,7 +282,7 @@ void ChangeGhostDirection(int ghostNum, int direction, int i, int j) {
 		else if (map[i + 1][j] > -1 && map[i + 1][j] != WALL && map[i + 1][j] < 4) direction = DOWN;
 		else if (map[i - 1][j] > -1 && map[i - 1][j] != WALL && map[i - 1][j] < 4) direction = UP;
 		else if (map[i][j - 1] > -1 && map[i][j - 1] != WALL && map[i][j - 1] < 4) direction = LEFT;
-		else STOP;
+		else direction = STOP;
 		break;
 	case DOWN:
 		if (map[i][j + 1] > -1 && map[i][j - 1] > -1 && map[i][j + 1] != WALL && map[i][j - 1] != WALL && map[i][j - 1] < 4 && map[i][j + 1] < 4) {
@@ -267,7 +293,7 @@ void ChangeGhostDirection(int ghostNum, int direction, int i, int j) {
 		else if (map[i][j - 1] > -1 && map[i][j - 1] != WALL && map[i][j - 1] < 4) direction = LEFT;
 		else if (map[i][j + 1] > -1 && map[i][j + 1] != WALL && map[i][j + 1] < 4) direction = RIGHT;
 		else if (map[i - 1][j] > -1 && map[i - 1][j] != WALL && map[i - 1][j] < 4) direction = UP;
-		else STOP;
+		else direction = STOP;
 		break;
 	case LEFT:
 		if (map[i + 1][j] > -1 && map[i - 1][j] > -1 && map[i + 1][j] != WALL && map[i - 1][j] != WALL && map[i - 1][j] < 4 && map[i + 1][j] < 4) {
@@ -278,27 +304,47 @@ void ChangeGhostDirection(int ghostNum, int direction, int i, int j) {
 		else if (map[i - 1][j] > -1 && map[i - 1][j] != WALL && map[i - 1][j] < 4) direction = UP;
 		else if (map[i + 1][j] > -1 && map[i + 1][j] != WALL && map[i + 1][j] < 4) direction = DOWN;
 		else if (map[i][j + 1] > -1 && map[i][j + 1] != WALL && map[i][j + 1] < 4) direction = RIGHT;
-		else STOP;
+		else direction = STOP;
 		break;
 	}
 	
 	switch (direction)
 	{
 	case UP:
+		if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+		else map[i][j] = EMPTY;
+
+		if (map[i - 1][j] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+		else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 		map[i - 1][j] = -1 * ghostNum;
-		map[i][j] = EMPTY;
 		break;
 	case RIGHT:
+		if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+		else map[i][j] = EMPTY;
+
+		if (map[i][j + 1] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+		else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 		map[i][j + 1] = -1 * ghostNum;
-		map[i][j] = EMPTY;
 		break;
 	case DOWN:
+		if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+		else map[i][j] = EMPTY;
+
+		if (map[i + 1][j] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+		else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 		map[i + 1][j] = -1 * ghostNum;
-		map[i][j] = EMPTY;
 		break;
 	case LEFT:
+		if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+		else map[i][j] = EMPTY;
+
+		if (map[i][j - 1] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+		else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 		map[i][j - 1] = -1 * ghostNum;
-		map[i][j] = EMPTY;
 		break;
 	case STOP:
 		break;
@@ -314,17 +360,27 @@ void MoveGhosts() {
 		int j = 0;
 		while (j < WIDTH) {
 			if (map[i][j] > 3 && map[i][j] < 8) {
-				int ghostNum = map[i][j];
+				int ghostNum = map[i][j]; // номер призрака на поле. Отнять 4 для номера в массиве
 				if ((i == 5 && j == 9) || (i == 5 && j == 10)) ghosts[ghostNum - 4].direction = UP;
 				int randNum = rand() % 3;
-				if (i == 7 && j > 2 && randNum == 1 && map[i + 1][j] != WALL && map[i + 1][j] > -1 && map[i + 1][j] < 4 && !((i == 5 && j == 9) || (i == 5 && j == 10)) && !((i == 4 && j == 9) || (i == 4 && j == 10))) {
+				if (i == 7 && j > 2 && j < 17 && randNum == 1 && map[i + 1][j] != WALL && map[i + 1][j] > -1 && map[i + 1][j] < 4 && !((i == 5 && j == 9) || (i == 5 && j == 10)) && !((i == 4 && j == 9) || (i == 4 && j == 10))) {
+					if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+					else map[i][j] = EMPTY;
+
+					if (map[i + 1][j] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+					else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 					map[i + 1][j] = -1 * ghostNum;
-					map[i][j] = EMPTY;
 					ghosts[ghostNum - 4].direction = DOWN;
 				}
-				else if (i == 7 && j > 2 && randNum == 0 && map[i][j + 1] != WALL && map[i][j + 1] > -1 && map[i][j + 1] < 4 && !((i == 5 && j == 9) || (i == 5 && j == 10)) && !((i == 4 && j == 9) || (i == 4 && j == 10))) {
+				else if (i == 5 && j == 1 && randNum == 0 && map[i][j + 1] != WALL && map[i][j + 1] > -1 && map[i][j + 1] < 4 && !((i == 5 && j == 9) || (i == 5 && j == 10)) && !((i == 4 && j == 9) || (i == 4 && j == 10))) {
+					if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+					else map[i][j] = EMPTY;
+
+					if (map[i][j + 1] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+					else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 					map[i][j + 1] = -1 * ghostNum;
-					map[i][j] = EMPTY;
 					ghosts[ghostNum - 4].direction = RIGHT;
 				}
 				else {
@@ -332,16 +388,26 @@ void MoveGhosts() {
 					case UP:
 						if (map[i - 1][j] == WALL || map[i-1][j] >= 4 || map[i - 1][j] < 0) ChangeGhostDirection(ghostNum, UP, i, j);
 						else {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i - 1][j] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i - 1][j] = -1 * ghostNum;
-							map[i][j] = EMPTY;
 						}
 						break;
 
 					case RIGHT:
 						if (map[i][j + 1] == WALL || map[i][j + 1] >= 4 || map[i][j + 1] < 0) ChangeGhostDirection(ghostNum, RIGHT, i, j);
 						else {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i][j + 1] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i][j + 1] = -1 * ghostNum;
-							map[i][j] = EMPTY;
 						}
 						break;
 
@@ -349,34 +415,71 @@ void MoveGhosts() {
 					case DOWN:
 						if (map[i + 1][j] == WALL || map[i + 1][j] >= 4 || map[i + 1][j] < 0) ChangeGhostDirection(ghostNum, DOWN, i, j);
 						else {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i + 1][j] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i + 1][j] = -1 * ghostNum;
-							map[i][j] = EMPTY;
 						}
 						break;
 
 					case LEFT:
 						if (map[i][j - 1] == WALL || map[i][j-1] >= 4 || map[i][j - 1] < 0) ChangeGhostDirection(ghostNum, LEFT, i, j);
 						else {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i][j - 1] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i][j - 1] = -1 * ghostNum;
-							map[i][j] = EMPTY;
 						}
 						break;
 					case STOP:
-						if (map[i - 1][j] == EMPTY || map[i - 1][j] == FOOD) {
+						if (map[i - 1][j] == EMPTY || map[i - 1][j] == FOOD || map[i - 1][j] == PLAYER) {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i - 1][j] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i - 1][j] = -1 * ghostNum;
 							ghosts[ghostNum - 4].direction = UP;
 						}
-						else if (map[i][j + 1] == EMPTY || map[i][j + 1] == FOOD) {
+						else if (map[i][j + 1] == EMPTY || map[i][j + 1] == FOOD || map[i][j + 1] == PLAYER) {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i][j + 1] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i][j + 1] = -1 * ghostNum;
 							ghosts[ghostNum - 4].direction = RIGHT;
 						}
-						else if (map[i + 1][j] == EMPTY || map[i + 1][j] == FOOD) {
+						else if (map[i + 1][j] == EMPTY || map[i + 1][j] == FOOD || map[i + 1][j] == PLAYER) {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i + 1][j] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i + 1][j] = -1 * ghostNum;
 							ghosts[ghostNum - 4].direction = DOWN;
 						}
-						else if (map[i][j - 1] == EMPTY || map[i][j - 1] == FOOD) {
+						else if (map[i][j - 1] == EMPTY || map[i][j - 1] == FOOD || map[i][j - 1] == PLAYER) {
+							if (ghosts[ghostNum - 4].onFoodFlag) map[i][j] = FOOD;
+							else map[i][j] = EMPTY;
+
+							if (map[i][j - 1] == FOOD) ghosts[ghostNum - 4].onFoodFlag = 1;
+							else ghosts[ghostNum - 4].onFoodFlag = 0;
+
 							map[i][j - 1] = -1 * ghostNum;
 							ghosts[ghostNum - 4].direction = LEFT;
+						}
+						else {
+							//продолжаем ждать
 						}
 					}
 				}
@@ -395,26 +498,56 @@ void MoveGhosts() {
 		while (j < WIDTH) {
 			if (map[i][j] == -1 * ENEMY1) {
 				map[i][j] = ENEMY1;
-				E1 = true;
 			}
 			else if (map[i][j] == -1 * ENEMY2) {
 				map[i][j] = ENEMY2;
-				E2 = true;
 			}
 			else if (map[i][j] == -1 * ENEMY3) {
 				map[i][j] = ENEMY3;
-				E3 = true;
 			}
 			else if (map[i][j] == -1 * ENEMY4) {
 				map[i][j] = ENEMY4;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	i = 0;
+	while (i < HEIGHT) {
+		int j = 0;
+		while (j < WIDTH) {
+			if (map[i][j] == ENEMY1) {
+				E1 = true;
+			}
+			else if (map[i][j] == ENEMY2) {
+				E2 = true;
+			}
+			else if (map[i][j] == ENEMY3) {
+				E3 = true;
+			}
+			else if (map[i][j] == ENEMY4) {
 				E4 = true;
 			}
 			j++;
 		}
 		i++;
 	}
-	if (!E1) map[5][8] = 4;
-	if (!E2) map[5][9] = 5;
-	if (!E3) map[5][10] = 6;
-	if (!E4) map[5][11] = 7;
+
+	if (!E1) {
+		map[5][8] = ENEMY1;
+		ghosts[0].onFoodFlag = 0;
+	}
+	if (!E2) {
+		map[5][9] = ENEMY2;
+		ghosts[1].onFoodFlag = 0;
+	}
+	if (!E3) {
+		map[5][10] = ENEMY3;
+		ghosts[2].onFoodFlag = 0;
+	}
+	if (!E4) {
+		map[5][11] = ENEMY4;
+		ghosts[3].onFoodFlag = 0;
+	}
 }
